16077c6e022e3ce676305579fd86bc021219b7e4
Neil Kistner <neil.kistner@gmail.com>
2017-04-13 19:46:15 -0500

    Cleanup testing process
    - Switch from pre/post npm-script hooks to bsb's js-post-build
    - Co-locate tests with source files, as they should be with Jest
    - Remove renderer util and bring in `bs-react-test-renderer`

---
+++ scripts/copy.js
@@ -0,0 +1,13 @@
+#!/usr/local/bin/node
+
+const fs = require('fs')
+const path = require('path')
+
+const root = path.join(__dirname, '..')
+const script = process.argv[2]
+const output = script.replace(path.join(root, 'lib', 'js'), '')
+
+fs.writeFileSync(
+  path.join(root, output),
+  fs.readFileSync(script)
+)
