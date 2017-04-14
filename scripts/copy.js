a92f023ab177a06756b1cfa7e736d53043d3773d
Neil Kistner <neil.kistner@gmail.com>
2017-04-14 08:05:31 -0500

    Better replacement

--- scripts/copy.js
+++ scripts/copy.js
@@ -6,8 +6,12 @@ const path = require('path')
 const root = path.join(__dirname, '..')
 const script = process.argv[2]
 const output = script.replace(path.join(root, 'lib', 'js'), '')
+const js = path.join(
+  path.dirname(script),
+  path.basename(script).replace(/^./, letter => letter.toLowerCase())
+)
 
 fs.writeFileSync(
   path.join(root, output),
-  fs.readFileSync(script.toLowerCase())
+  fs.readFileSync(js)
 )
